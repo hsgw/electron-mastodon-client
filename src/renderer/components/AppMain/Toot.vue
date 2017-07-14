9c1c4c030cad674f414ade173b39d58cfd44f495
hsgw <urkwtky@gmail.com>
2017-07-15 00:09:50 +0900

    ブースト表示の修正

--- src/renderer/components/AppMain/Toot.vue
+++ src/renderer/components/AppMain/Toot.vue
@@ -1,37 +1,42 @@
 <template>
   <div class="toot">
-    <div class="avatar">
-      <div>
-        <img :src="toot.account.avatar">
-      </div>
-      <div class="tootType" :class="tootTypeClass">
-        <icon class="icon" :name="tootType"></icon>
-      </div>
+    <div v-if="isReblog" class="reblog">
+      <a :href="toot.account.url" target="_blank">{{ '@' + toot.account.acct }} </a>からのブースト
     </div>
-    <div class="content">
-      <div class="header">
-        <div class="name">
-          <a :href="toot.account.url" target="_blank">
-            <div class="display" v-emoji-render:data="toot.account.display_name"></div>
-            <div class="user">{{ '@'+toot.account.acct }}</div>
+    <div class="body">
+      <div class="avatar">
+        <div>
+          <img :src="account.avatar">
+        </div>
+        <div class="tootType" :class="tootTypeClass">
+          <icon class="icon" :name="tootType"></icon>
+        </div>
+      </div>
+      <div class="content">
+        <div class="header">
+          <div class="name">
+            <a :href="account.url" target="_blank">
+              <div class="display" v-emoji-render:data="account.display_name"></div>
+              <div class="user">{{ '@'+account.acct }}</div>
+            </a>
+          </div>
+          <a :href="toot.url" target="_blank" class="date">
+            {{ getDate }}<br> {{ getTime }}
           </a>
         </div>
-        <a :href="toot.url" target="_blank" class="date">
-          {{ getDate }}<br> {{ getTime }}
-        </a>
-      </div>
-      <div v-if="hasSpoiler" class="status spoiler">
-      <span v-emoji-render:spoiler="toot.spoiler_text"></span>
-      <button @click="toggleSpoiler">...</button>
-      </div>
-      <div v-show="isOpened" class="status" v-emoji-render:status="toot.content"></div>
-      <media-attachments :attachments="toot.media_attachments" :sensitive="(toot.sensitive !== true)"></media-attachments>
-      <div class="status_action">
-        <div @click="reply"><icon class="icon reply" name="reply" scale="0.9"></icon></div>
-        <div @click="boost(toot.id, toot.uri, !toot.reblogged)"><icon class="icon boost" :class="{on:toot.reblogged, off:isSendingBoost}" name="retweet" scale="0.9"></icon></div>
-        <div @click="favorite(toot.id, toot.uri, !toot.favourited)"><icon class="icon fav" :class="{on:toot.favourited, off:isSendingFav}" name="star" scale="0.9"></icon></div>
-        <div v-if="isMyToot" @click="deleteToot(toot.id)"><icon class="icon del"  name="trash" scale="0.9"></icon></div>
-        <!--<div><icon class="icon bar" name="bars"></icon></div>-->
+        <div v-if="hasSpoiler" class="status spoiler">
+        <span v-emoji-render:spoiler="toot.spoiler_text"></span>
+        <button @click="toggleSpoiler">...</button>
+        </div>
+        <div v-show="isOpened" class="status" v-emoji-render:status="toot.content"></div>
+        <media-attachments :attachments="toot.media_attachments" :sensitive="(toot.sensitive !== true)"></media-attachments>
+        <div class="status_action">
+          <div @click="reply"><icon class="icon reply" name="reply" scale="0.9"></icon></div>
+          <div @click="boost(toot.id, toot.uri, !toot.reblogged)"><icon class="icon boost" :class="{on:toot.reblogged, off:isSendingBoost}" name="retweet" scale="0.9"></icon></div>
+          <div @click="favorite(toot.id, toot.uri, !toot.favourited)"><icon class="icon fav" :class="{on:toot.favourited, off:isSendingFav}" name="star" scale="0.9"></icon></div>
+          <div v-if="isMyToot" @click="deleteToot(toot.id)"><icon class="icon del"  name="trash" scale="0.9"></icon></div>
+          <!--<div><icon class="icon bar" name="bars"></icon></div>-->
+        </div>
       </div>
     </div>
   </div>
@@ -50,6 +55,8 @@ export default {
   },
   data() {
     return {
+      account: this.toot.reblog != null ? this.toot.reblog.account : this.toot.account,
+      isReblog: this.toot.reblog != null,
       hasSpoiler: false,
       isOpened: true,
       isSendingFav: false,
@@ -69,6 +76,7 @@ export default {
       return this.toot.account.acct === this.$store.state.mastodon.myAccount.acct;
     },
     tootType() {
+      if (this.toot.reblog != null) return 'retweet';
       if (this.toot.in_reply_to_id != null) return 'reply';
       switch (this.toot.visibility) {
         case 'public':
@@ -85,6 +93,7 @@ export default {
       }
     },
     tootTypeClass() {
+      if (this.toot.reblog != null) return 'retweet';
       if (this.toot.in_reply_to_id != null) return 'reply';
       switch (this.toot.visibility) {
         case 'public':
@@ -180,90 +189,98 @@ export default {
 
 .toot {
   vertical-align: text-bottom;
-  display: flex;
   background-color: $content-bg-color;
   margin: 0 10px 0 5px;
   padding: 1em 0.5em 0.5em 0.5em;
   border-bottom-color: $bg-color;
   border-bottom-style: solid;
   border-bottom-width: 1px;
-
-  .avatar {
+  .reblog {
+    color: $hidden-color;
+    padding-bottom: 0.5em;
+  }
+  .body {
     display: flex;
-    flex-direction: column;
-    div { 
-      text-align: center;
-      img {
-        width:32px;
-        height:32px;
-        margin-bottom: 5px;
+    .avatar {
+      display: flex;
+      flex-direction: column;
+      div { 
+        text-align: center;
+        img {
+          width:32px;
+          height:32px;
+          margin-bottom: 5px;
+        }
+      }
+      .tootType {
+        &.public {
+          color: darken($hidden-color, 50%);
+        }
+        &.reply {
+          color: $success-color;
+        }
+        &.retweet {
+          color: darken($success-color, 30%);
+        }
       }
     }
-    .tootType {
-      &.public {
-        color: darken($hidden-color, 50%);
-      }
-      &.reply {
-        color: $success-color;
+    .spoiler {
+      button {
+        margin-left:5px;
       }
     }
-  }
-  .spoiler {
-    button {
-      margin-left:5px;
-    }
-  }
-  .content {
-    width: 100%;
-    margin-left: 0.5em;
-    .header {
-      display:flex;
-      flex-wrap: wrap;
-      .name {
-        a:hover {
-          text-decoration: none;
+    .content {
+      width: 100%;
+      margin-left: 0.5em;
+      .header {
+        display:flex;
+        flex-wrap: wrap;
+        .name {
+          a:hover {
+            text-decoration: none;
+          }
+          @mixin ellipsis {
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+          }
+          .display {
+            @include ellipsis;
+            max-width: 180px;
+          }
+          .user {
+            @include ellipsis;
+            max-width: 180px;
+            color: grey;
+            font-size: smaller;
+          }
         }
-        @mixin ellipsis {
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-        }
-        .display {
-          @include ellipsis;
-          max-width: 180px;
-        }
-        .user {
-          @include ellipsis;
-          max-width: 180px;
+        .date {
+          margin-left: auto;
           color: grey;
-          font-size: smaller;
+          font-size: x-small;
         }
-      }
-      .date {
-        margin-left: auto;
-        color: grey;
-        font-size: x-small;
       }
     }
-  }
-  .status_action {
-    margin-top: 1em;
-    display: flex;
-    justify-content: space-between;
-    width: 120px;
-    line-height: 0.8em;
-    div {
-      cursor: pointer;
-      .icon {
-        &.off {
-          cursor:  not-allowed;
-          color:$hidden-color;
-        }
-        &.fav.on {
-          color:$fav-color;
-        }
-        &.boost.on {
-          color:$reblog-color;
+    .status_action {
+      margin-top: 1em;
+      display: flex;
+      justify-content: space-between;
+      width: 120px;
+      line-height: 0.8em;
+      div {
+        cursor: pointer;
+        .icon {
+          &.off {
+            cursor:  not-allowed;
+            color:$hidden-color;
+          }
+          &.fav.on {
+            color:$fav-color;
+          }
+          &.boost.on {
+            color:$reblog-color;
+          }
         }
       }
     }
